6873ef406227f61c95d50f02dd695cf4767d0c65
Hugh Saunders <hugh@wherenow.org>
2014-12-30 10:28:38 +0000

    Retry exec command via ssh
    Adds config variable ssh_connection > exec_retry. This defines the
    number of times exec_command will be retried when using the openssh
    connection plugin. exec_command will only be retried if the previous
    attempt exited with status 255.

--- lib/ansible/constants.py
+++ lib/ansible/constants.py
@@ -165,6 +165,7 @@ DEFAULT_LOAD_CALLBACK_PLUGINS  = get_config(p, DEFAULTS, 'bin_ansible_callbacks'
 ANSIBLE_SSH_ARGS               = get_config(p, 'ssh_connection', 'ssh_args', 'ANSIBLE_SSH_ARGS', None)
 ANSIBLE_SSH_CONTROL_PATH       = get_config(p, 'ssh_connection', 'control_path', 'ANSIBLE_SSH_CONTROL_PATH', "%(directory)s/ansible-ssh-%%h-%%p-%%r")
 ANSIBLE_SSH_PIPELINING         = get_config(p, 'ssh_connection', 'pipelining', 'ANSIBLE_SSH_PIPELINING', False, boolean=True)
+ANSIBLE_SSH_EXEC_RETRY         = get_config(p, 'ssh_connection', 'exec_retry', 'ANSIBLE_SSH_EXEC_RETRY', 1, integer=True)
 PARAMIKO_RECORD_HOST_KEYS      = get_config(p, 'paramiko_connection', 'record_host_keys', 'ANSIBLE_PARAMIKO_RECORD_HOST_KEYS', True, boolean=True)
 # obsolete -- will be formally removed in 1.6
 ZEROMQ_PORT                    = get_config(p, 'fireball_connection', 'zeromq_port', 'ANSIBLE_ZEROMQ_PORT', 5099, integer=True)
